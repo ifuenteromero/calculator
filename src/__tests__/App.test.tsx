560a59973523b55b0207bfa4c489f203acc1f745
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-02-22 14:19:13 +0100

    Initial commit

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from '../components/App';
+
+test('shows title', () => {
+    render(<App />);
+    const title = screen.getByRole('heading');
+    expect(title.textContent).toMatch('calculator');
+});
